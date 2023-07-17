db612461e52f179ec3a25699d8d89bd3066a3903
unknown <yacineboudebbouz7@gmail.com>
2023-07-17 13:51:27 +0200

    profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -149,6 +149,48 @@ class _ProfilePageState extends State<ProfilePage> {
           ],
         ),
       ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 170),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 200,
+              color: Colors.grey[900],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Full Name',
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  _userName,
+                  style: const TextStyle(fontSize: 17),
+                )
+              ],
+            ),
+            const Divider(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Email',
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(
+                  _email,
+                  style: const TextStyle(fontSize: 17),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
